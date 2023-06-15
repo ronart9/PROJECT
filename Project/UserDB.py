c83e697c3741a75af27aa7bc9c5463fa063bf074
Ronart9 <ronhakim9@gmail.com>
2023-06-15 03:04:52 +0300

    CHANGE

--- Project/UserDB.py
+++ Project/UserDB.py
@@ -2,16 +2,16 @@ import sqlite3
 import hashlib
 
 class UserDB(object):
-    def __init__(self, tablename = "Users", Id = "Id", email = "email", password = "password", username ="username", countwins= "countwins"):
+    def __init__(self, tablename="Users", Id="Id", email="email", password="password", username="username", countwins="countwins"):
         self.__tablename = tablename
         self.__Id = Id
         self.__email = email
         self.__password = password
         self.__username = username
-        self.__countwins= countwins
+        self.__countwins = countwins
 
         conn = sqlite3.connect('test.db')
-        print ("Opened database successfully")
+        print("Opened database successfully")
         str = "CREATE TABLE IF NOT EXISTS " + self.__tablename + "(" + self.__Id + " " + " INTEGER PRIMARY KEY AUTOINCREMENT ,"
         str += " " + self.__email + " TEXT    NOT NULL ,"
         str += " " + self.__password + " TEXT    NOT NULL, "
@@ -21,18 +21,19 @@ class UserDB(object):
         print("Table created successfully")
         conn.commit()
         conn.close()
+
     def get_table_name(self):
         return self.__tablename
 
-    def get_win(self, usarname):
+    def get_win(self, username):
         try:
             conn = sqlite3.connect('test.db')
             print("Opened database successfully")
-            strsql = "SELECT * from " + self.__tablename + " where " + self.__username + "=" + "'" + str(usarname) + "'"
+            strsql = f"SELECT * from {self.__tablename} where {self.__username} = ?"
             print(strsql)
-            cursor = conn.execute(strsql)
+            cursor = conn.execute(strsql, (username,))
             print(cursor)
-            row= cursor.fetchone()
+            row = cursor.fetchone()
             print(row)
             user_data = str([row[1], row[2], row[3], row[4]])
             print("User data: " + str(user_data))
@@ -41,32 +42,29 @@ class UserDB(object):
 
             return row[4]
         except:
-            print("failed")
+            print("Failed")
             return False
-
-
 
     def update_wins(self, username):
         try:
             conn = sqlite3.connect('test.db')
             cur_wins = self.get_win(username)
-            updated_wins= int(cur_wins) + 1
-            print("new count wins = "+ str(updated_wins))
-            str_update= f"update {self.__tablename} set {self.__countwins} = '{updated_wins}' where {self.__username} = '{username}'"
+            updated_wins = int(cur_wins) + 1
+            print("new count wins =", updated_wins)
+            str_update = f"UPDATE {self.__tablename} SET {self.__countwins} = ? WHERE {self.__username} = ?"
             print(str_update)
-            conn.execute(str_update)
+            conn.execute(str_update, (updated_wins, username))
             conn.commit()
             conn.close()
-            print("success on updating wins !")
+            print("Success on updating wins!")
         except:
-            print("Failed updating wins !")
-
+            print("Failed updating wins!")
 
     def select_all_users(self):
         try:
             conn = sqlite3.connect('test.db')
             print("Opened database successfully")
-            str1 = "select*from " + self.__tablename
+            str1 = f"SELECT * FROM {self.__tablename}"
             print(str1)
             cursor = conn.execute(str1)
             rows = cursor.fetchall()
@@ -78,17 +76,17 @@ class UserDB(object):
             return arr_users
         except:
             return False
+
     def insert_user(self, email, password, username):
         try:
             conn = sqlite3.connect('test.db')
             salt = 'SltKey'
             slt_pass = hashlib.md5(salt.encode('utf-8') + password.encode('utf-8')).hexdigest()
             print(slt_pass)
-            str_insert = f"INSERT INTO {self.__tablename} ({self.__email}, {self.__password}," \
-                         f"{self.__username}, {self.__countwins}) VALUES ('{email}', '{slt_pass}'," \
-                         f" '{username}', '{0}')"
+            str_insert = f"INSERT INTO {self.__tablename} ({self.__email}, {self.__password}, {self.__username}, {self.__countwins}) VALUES (?, ?, ?, ?)"
+            values = (email, slt_pass, username, 0)
             print(str_insert)
-            conn.execute(str_insert)
+            conn.execute(str_insert, values)
             print("1")
             conn.commit()
             print("2")
@@ -102,27 +100,29 @@ class UserDB(object):
     def delete_by_firstname(self, email):
         try:
             conn = sqlite3.connect('test.db')
-            str_delete = "DELETE  from " + self.__tablename + " where " + self.__email + "=" + "'"+str(email)+"'"
+            str_delete = f"DELETE from {self.__tablename} where {self.__email} = ?"
             print(str_delete)
-            conn.execute(str_delete)
+            conn.execute(str_delete, (email,))
             conn.commit()
             conn.close()
             print("Record deleted successfully")
             return "Success"
         except:
             return "Failed to delete user"
+
     def return_user_by_email(self, email, password):
         try:
             conn = sqlite3.connect('test.db')
             print("Opened database successfully")
-            salt= 'SltKey'
-            slt_password= hashlib.md5(salt.encode('utf-8')+ password.encode('utf-8')).hexdigest()
+            salt = 'SltKey'
+            slt_password = hashlib.md5(salt.encode('utf-8') + password.encode('utf-8')).hexdigest()
             print(slt_password)
-            strsql = "SELECT * from " + self.__tablename + " where " + self.__email + "=" + "'" + str(email) + "'" + " and " + self.__password + "=" + "'" + slt_password + "'"
+            strsql = f"SELECT * from {self.__tablename} where {self.__email} = ? and {self.__password} = ?"
+            values = (email, slt_password)
             print(strsql)
-            cursor = conn.execute(strsql)
+            cursor = conn.execute(strsql, values)
             print(cursor)
-            row= cursor.fetchone()
+            row = cursor.fetchone()
             print(row)
             user_data = str([row[1], row[2], row[3], row[4]])
             print("User data: " + str(user_data))
@@ -131,20 +131,21 @@ class UserDB(object):
 
             return row[3]
         except:
-            print("failed")
+            print("Failed")
             return False
 
     def log_in(self, email, password):
         try:
             conn = sqlite3.connect('test.db')
-            strsql = "SELECT * FROM " + self.__tablename + " WHERE " + self.__email + "=" + "'" + str(email) + "'"+ " and " + self.__password + "=" + "'" + str(password) + "'"
+            strsql = f"SELECT * FROM {self.__tablename} WHERE {self.__email} = ? and {self.__password} = ?"
+            values = (email, password)
             print(strsql)
-            cursor = conn.execute(strsql)
+            cursor = conn.execute(strsql, values)
             row = cursor.fetchone()
             conn.commit()
             conn.close()
             if row:
-                print("success")
+                print("Success")
                 return True
             else:
                 print("Failed to find user")
@@ -152,25 +153,11 @@ class UserDB(object):
         except:
             return False
 
-       # except:
-            #messagebox.showerror("error message", "Error")
-            #return
-
-
-
     def __str__(self):
-        return "table  name is ", self.__tablename
+        return "Table name is " + self.__tablename
 
 
-#u=UserDB()
-#u.insert_user("u@x.com", "oron", 'yaron', 69)
-#u.insert_user("v@y.com", "dvidi", 'davidi')
-#u.insert_user("t@a.com", "aba", 'origin')
+# Example usage
+#u = UserDB()
+#u.insert_user("u@x.com", "oron", 'yaron')
 #u.select_all_users()
-#u.check_user_by_username("Asaf")
-#u.delete_username("Asaf")
-#u.check_user_by_username("Asaf")
-#user=u.return_user_by_email('tom@g', "123")
-#print(user[0])
-#print(user)
-#end
